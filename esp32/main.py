31fd22d0ebf5a5d02b7022cafd9927a1c718743c
Marcos Tullio <marcostullio.s@gmail.com>
2024-07-31 23:14:52 -0300

    Código atualizado

--- esp32/main.py
+++ esp32/main.py
@@ -3,51 +3,46 @@ import socket
 import dht
 import machine
 import time
+import ujson as json  # JSON no MicroPython
 
 # Configurações da rede Wi-Fi
-ssid = 'Amanda'
-password = '31367783'
+ssid = 'SUA_REDE_WIFI'
+password = 'SUA_SENHA'
 
 station = network.WLAN(network.STA_IF)
 station.active(True)
 station.connect(ssid, password)
 
-# Aguarde até que a conexão Wi-Fi esteja estabelecida
+# Aguarda até que a conexão Wi-Fi esteja estabelecida
 while not station.isconnected():
     pass
 
 print('Conexão estabelecida')
 print(station.ifconfig())
 
-# Inicialize o sensor DHT11 e o LED
+# Inicializa o sensor DHT11 e o LED
 sensor = dht.DHT11(machine.Pin(4))
 LED = machine.Pin(2, machine.Pin.OUT)
 
-# Configure o servidor web
+# Configura o servidor web
 addr = socket.getaddrinfo('0.0.0.0', 8080)[0][-1]
 server = socket.socket()
 server.bind(addr)
 server.listen(1)
 print('Servidor ouvindo na porta 8080')
 
-def web_page(temp, hum):
-    html = """<html>
-        <head>
-            <title>Leitura do Sensor DHT11</title>
-        </head>
-        <body>
-            <h1>Leitura do Sensor DHT11</h1>
-            <p>Temperatura: {} &deg;C</p>
-            <p>Umidade: {} %</p>
-        </body>
-    </html>""".format(temp, hum)
-    return html
+def json_response(temp, hum):
+    data = {
+        'temperature': float(temp),
+        'humidity': float(hum)
+    }
+    return json.dumps(data)
 
 def blink_LED():
     for i in range(3):
         LED.value(not LED.value())
-        time.sleep(0.5)  # Tempo reduzido para um piscar mais rápido
-    LED.value(0)  # Desliga o LED após piscar
+        time.sleep(0.5) 
+    LED.value(0)  # Desliga o LED
 
 while True:
     try:
@@ -57,18 +52,18 @@ while True:
         request = str(request)
         print('Conteúdo do request =', request)
         
-        # Piscar o LED ao receber uma solicitação
+        # Pisca o LED ao receber uma solicitação
         blink_LED()
         
-        # Leia os dados do sensor
+        # Ler os dados do sensor
         sensor.measure()
         temp = sensor.temperature()
         hum = sensor.humidity()
         
-        # Envie a resposta ao cliente
-        response = web_page(temp, hum)
+        # Enva a resposta ao cli
+        response = json_response(temp, hum)
         client.send('HTTP/1.1 200 OK\n')
-        client.send('Content-Type: text/html\n')
+        client.send('Content-Type: application/json\n')  # Definindo o tipo de conteúdo como JSON
         client.send('Connection: close\n\n')
         client.sendall(response)
         client.close()
@@ -76,3 +71,4 @@ while True:
     except OSError as e:
         client.close()
         print('Erro no cliente:', e)
+
